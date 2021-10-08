29d0cb52b6d201e7e988d1fef15854f4dfc57c7c
stefanbreitenstein <stefan.breitenstein@tt-s.com>
2021-10-08 08:16:00 +0200

    add user agent

--- src/datasources/openSubtitlesApi.js
+++ src/datasources/openSubtitlesApi.js
@@ -4,19 +4,21 @@ class OpenSubtitlesApi extends RESTDataSource {
   constructor() {
     super();
     this.baseURL = 'https://api.opensubtitles.com/api/v1/';
-    this.headers = {'Api-Key': OPEN_SUBTITLES_API_KEY};
+    this.headers = {
+      'Api-Key': OPEN_SUBTITLES_API_KEY,
+      'User-Agent': 'plussub-1.1.0'
+    };
   }
 
   async search({tmdb_id, language, season_number, episode_number}) {
     const seasonNumberObj = season_number ? {season_number} : {};
     const episodeNumberObj = episode_number ? {episode_number} : {};
-
     return this.get(`subtitles`, {
-      tmdb_id,
-      order_by: 'ratings',
+      ...episodeNumberObj,
       languages: language,
+      order_by: 'ratings',
       ...seasonNumberObj,
-      ...episodeNumberObj
+      tmdb_id,
     }, {headers: this.headers});
   }
 }
