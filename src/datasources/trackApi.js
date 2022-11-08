d19419d59b3f44a5f7f8553e3e3006a96090e0b6
stefanbreitenstein <stefan.breitenstein@tt-s.com>
2022-11-08 08:27:39 +0100

    use another analytics tool

--- src/datasources/trackApi.js
+++ src/datasources/trackApi.js
@@ -4,14 +4,19 @@ import { RESTDataSource } from 'apollo-datasource-rest';
 export class TrackApi extends RESTDataSource {
   constructor() {
     super();
-    this.baseURL = 'https://le4lquqmc1.execute-api.eu-west-1.amazonaws.com';
+    this.baseURL = 'https://heapanalytics.com';
   }
 
   async track({origin, language, source}) {
-    return this.post('/', {
-      origin,
-      source,
-      language
+    return this.post('/api/track', {
+      app_id: HEAP_APP_ID,
+      event: "track",
+      identity: "unknown",
+      properties: {
+        origin,
+        source,
+        language
+      }
     });
   }
 }
